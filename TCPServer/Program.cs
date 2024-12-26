111fb685565030b65be49865d03134e4a282ae99
giokovacevic <kovacevico012@gmail.com>
2024-12-26 11:38:06 +0100

    UDPServer i TCPServer umesto jednog Servera

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client
+namespace TCPServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo");
         }
     }
 }
